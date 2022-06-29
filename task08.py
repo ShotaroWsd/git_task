75d928c99d132b6775158fa6b24f1ab98f6d699c
ShotaroWsd <0kz46227329345e@gmail.com>
2022-06-29 10:52:53 +0900

    add class to calculate sum

--- task08.py
+++ task08.py
@@ -1,3 +1,4 @@
+#スーパークラス　継承して四則演算他の計算と出力を行う
 class Base:   
     result = 0
     operation = ''
@@ -9,11 +10,20 @@ class Base:
     def print_result(self):
         print(self.operation, self.result)
 
+#和 a + b
+class sum(Base):
+    operation = 'a + b ='
+
+    def __init__(self, a, b):
+        super().__init__(a, b)
+        self.result = self.a + self.b
+
 
 def main():
     a = int(input('Input integer a: '))
     b = int(input('Input integer b: '))
 
+    sum(a, b).print_result()
 
 if __name__ == '__main__':
     main()
